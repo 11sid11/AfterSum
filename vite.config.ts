f20eb44cfb49ec441fd0850e305df899bcb4e9b5
Siddharth Sahoo <67169975+11sid11@users.noreply.github.com>
2026-08-16 01:56:15 +0530

    switch PWA updates to user prompt

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       includeAssets: [
         'favicon-64.png',
         'apple-touch-icon.png',
